3d0db2a88b6bed2a4cf4b9a9e9446eecb2ec8a96
Pavel Semenchenko <imagemakerspa@icloud.com>
2022-10-24 23:48:36 +0300

    try to write code in defaults

---
+++ wind2wind/Screens/SecureVC.swift
@@ -0,0 +1,21 @@
+//
+//  SecureVC.swift
+//  wind2wind
+//
+//  Created by Pavel Semenchenko on 24.10.2022.
+//
+
+import Foundation
+import UIKit
+
+class SecureVC: UIViewController {
+    
+    @IBOutlet weak var secureText: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        secureText.text = UserDefaults.standard.value(forKey: "codeIn") as! String
+    }
+    
+    
+}
---
+++ wind2wind/Screens/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  wind2wind
+//
+//  Created by Pavel Semenchenko on 24.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var check: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let codeIn = textField.text
+        UserDefaults.standard.set(codeIn, forKey: "codeIn")
+        
+        /*
+        if let wellcome = UserDefaults.standard.value(forKey: "codeIn") as? String {
+         check.text = wellcome
+         } else {
+         UserDefaults.standard.set("zeroIn", forKey: "codeIn")
+         }*/
+         
+        
+    }
+    
+    @IBAction func pushClicked(_ sender: Any) {
+        
+        check.text = UserDefaults.standard.value(forKey: "codeIn") as! String
+        // let passwordScreen = SecureVC()
+        // navigationController?.pushViewController(passwordScreen, animated: true)
+    }
+    
+}
+
