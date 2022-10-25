61e234a97144de32631c4edb3a90dd8c9ec664e2
Pavel Semenchenko <imagemakerspa@icloud.com>
2022-10-25 11:31:32 +0300

    cleared button

--- wind2wind/Screens/ViewController.swift
+++ wind2wind/Screens/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         /*
         if let wellcome = UserDefaults.standard.value(forKey: "codeIn") as? String {
             check.text = wellcome
